3ac0306d21a994e8ef44bc02f6005762bde55080
alsanchez86 <sanchezpizarro86@gmail.com>
2018-04-06 14:24:55 +0200

    compile with gulp

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,3 +1,29 @@
+/*
+    Cache read only
+*/
+// define(["jquery"], function ($) {
+//     // Private vars
+//     var _this = {};
+
+//     // Public vars
+//     var cache = {};
+
+//     // Private Methods
+//     function _set(selector) {
+//         _this[selector] = $(selector);
+//     }
+
+//     // Public Methods
+//     cache.get = function (selector, force) {
+//         if (_this[selector] !== undefined && force === undefined) {
+//             return _this[selector];
+//         }
+//         _set(selector);
+//         return _this[selector];
+//     }
+
+//     return cache;
+// });
 "use strict";
 
 console.log("main module");
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     runSequence = require('run-sequence'),
     fs = require('fs'),
@@ -40,6 +42,70 @@ gulp.task('css:min', function () {
         );
 });
 
+gulp.task('js:lib', function () {
+    return gulp.src([
+            // jquery
+            pkg.node_modules + "jquery/dist/jquery.min.js",
+            // popper
+            pkg.node_modules + 'popper.js/dist/umd/popper.min.js',
+            // bootstrap
+            pkg.node_modules + 'bootstrap/dist/js/bootstrap.min.js'
+        ])
+        .pipe(
+            concat("lib.js")
+        )
+        .pipe(
+            gulp.dest(pkg.dist + 'js/')
+        );
+});
+
+gulp.task('js:custom', function () {
+    return gulp.src([
+            pkg.js + 'lib/jquery_cache.js',
+            pkg.js + 'main.js'
+        ])
+        .pipe(
+            concat("custom.js")
+        )
+        .pipe(
+            gulp.dest(pkg.dist + 'js/')
+        );
+});
+
+gulp.task('js:concat', function() {
+    return gulp.src([
+            pkg.dist + 'js/lib.js',
+            pkg.dist + 'js/custom.js'
+        ])
+        .pipe(
+            concat("main.js")
+        )
+        .pipe(
+            gulp.dest(pkg.dist + 'js/')
+        );
+});
+
+gulp.task('js:min', function() {
+    return gulp.src([
+            pkg.dist + 'js/**/*.js',
+            '!' + pkg.dist + 'js/**/*.min.js'
+        ])
+        .pipe(
+            uglify()
+                .on('error', function(e){
+                    console.log(e);
+                })
+        )
+        .pipe(
+            rename({
+                suffix: '.min'
+            })
+        )
+        .pipe(
+            gulp.dest(pkg.dist + 'js/')
+        );
+});
+
 // Tasks
 gulp.task('watch', ['default'], function () {
     gulp.watch([
@@ -48,13 +114,13 @@ gulp.task('watch', ['default'], function () {
 });
 
 gulp.task('dev', [], function (done) {
-    runSequence('css:sass', function () {
+    runSequence('css:sass', 'js:lib', 'js:custom', 'js:concat', function () {
         done();
     });
 });
 
 gulp.task('prod', [], function (done) {
-    runSequence('css:sass', 'css:min', function () {
+    runSequence('css:sass', 'css:min', 'js:lib', 'js:custom', 'js:concat', 'js:min', function () {
         done();
     });
 });
