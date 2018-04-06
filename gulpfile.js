e37d0e96f152764cbb3c91bfce4d7ce20b0f550a
alsanchez86 <sanchezpizarro86@gmail.com>
2018-04-06 12:11:50 +0200

    add SCSS dependencies

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    runSequence = require('run-sequence'),
+    fs = require('fs'),
+    pkg = JSON.parse(fs.readFileSync('./package.json'));
+
+gulp.task('css:sass', function () {
+    return gulp.src([
+            pkg.css + 'sass/main.scss'
+        ])
+        .pipe(
+            sass.sync()
+            .on('error', sass.logError)
+        )
+        .pipe(
+            gulp.dest(pkg.dist + 'css/')
+        );
+});
+
+gulp.task('css:min', function () {
+    return gulp.src([
+            pkg.dist + 'css/main.css'
+        ])
+        .pipe(
+            cleanCSS({
+                compatibility: 'ie8'
+            })
+        )
+        .pipe(
+            rename({
+                suffix: '.min'
+            })
+        )
+        .pipe(
+            gulp.dest(pkg.dist + 'css/')
+        );
+});
+
+// Tasks
+gulp.task('watch', ['default'], function () {
+    gulp.watch([
+        pkg.css + 'sass/**/*.scss'
+    ], ['default']);
+});
+
+gulp.task('dev', [], function (done) {
+    runSequence('css:sass', function () {
+        done();
+    });
+});
+
+gulp.task('prod', [], function (done) {
+    runSequence('css:sass', 'css:min', function () {
+        done();
+    });
+});
+
+gulp.task('default', ["dev"]);
