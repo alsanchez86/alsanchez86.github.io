c4df18e7b2ae904f2a66030278e24af8b483b35e
alsanchez86 <sanchezpizarro86@gmail.com>
2018-04-08 21:11:22 +0200

    body hidden mobile menu open

--- dist/js/main.js
+++ dist/js/main.js
@@ -47,14 +47,17 @@
 (function () {
     var nav_link_as_icon = $("#nav-link-as-icon");
     var header_page = $("#header-page");
+    var body = $("body");
 
     nav_link_as_icon.bind("click", function () {
         $(this).data("is-open", !$(this).data("is-open"));
 
         if ($(this).data("is-open")) {
             header_page.addClass("header-page-open");
+            body.addClass("hidden");
         }else{
             header_page.removeClass("header-page-open");
+            body.removeClass("hidden");
         }
     });
 })();
