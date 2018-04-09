cf123510217489ac59ac7a4e75acb62cfcf64722
alsanchez86 <sanchezpizarro86@gmail.com>
2018-04-09 12:01:01 +0200

    add loader spinner

--- dist/js/main.js
+++ dist/js/main.js
@@ -45,22 +45,50 @@
 // Importar y usar jquery cache
 
 (function () {
-    var nav_link_as_icon = $("#nav-link-as-icon");
-    var header_page = $("#header-page");
     var body = $("body");
     var html = $("html");
+    var nav_link_as_icon = $("#nav-link-as-icon");
+    var header_page = $("#header-page");
+
+    function _page_hidden(is_hidden) {
+        if (is_hidden) {
+            body.addClass("hidden");
+            html.addClass("hidden");
+        } else {
+            body.removeClass("hidden");
+            html.removeClass("hidden");
+        }
+    }
+
+    function _page_loading(is_loading) {
+        var content = $("#content");
+        var spinner = $("#spinner");
+
+        _page_hidden(is_loading);
+
+        if (is_loading) {
+            content.addClass("d-none");
+        } else {
+            content.removeClass("d-none");
+            spinner.addClass("d-none");
+        }
+    }
 
     nav_link_as_icon.bind("click", function () {
         $(this).data("is-open", !$(this).data("is-open"));
 
+        _page_hidden($(this).data("is-open"));
+
         if ($(this).data("is-open")) {
             header_page.addClass("header-page-open");
-            body.addClass("hidden");
-            html.addClass("hidden");
-        }else{
+        } else {
             header_page.removeClass("header-page-open");
-            body.removeClass("hidden");
-            html.removeClass("hidden");
         }
     });
+
+    _page_loading(true);
+
+    setTimeout(function () {
+        _page_loading(false);
+    }, 2000);
 })();
