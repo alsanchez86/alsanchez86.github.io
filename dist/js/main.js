bad7c7b93353385ac250228ab10844920e67aeb7
alsanchez86 <sanchezpizarro86@gmail.com>
2018-04-14 17:06:49 +0200

    add animate.css

--- dist/js/main.js
+++ dist/js/main.js
@@ -44,38 +44,67 @@
 
 // Importar y usar jquery cache
 
+$.fn.extend({
+    animateCss: function (animationName, callback) {
+        var animationEnd = (function (el) {
+            var animations = {
+                animation: 'animationend',
+                OAnimation: 'oAnimationEnd',
+                MozAnimation: 'mozAnimationEnd',
+                WebkitAnimation: 'webkitAnimationEnd',
+            };
+
+            for (var t in animations) {
+                if (el.style[t] !== undefined) {
+                    return animations[t];
+                }
+            }
+        })(document.createElement('div'));
+
+        this.addClass('animated ' + animationName).one(animationEnd, function () {
+            $(this).removeClass('animated ' + animationName);
+
+            if (typeof callback === 'function') callback();
+        });
+
+        return this;
+    },
+});
+
 (function () {
     var body = $("body");
     var html = $("html");
     var nav_link_as_icon = $("#nav-link-as-times");
     var header_page = $("#header-page");
 
-    function _page_hidden(is_hidden) {
+    function _page_overflow_hidden(is_hidden) {
         if (is_hidden) {
-            body.addClass("hidden");
-            html.addClass("hidden");
+            body.css({overflow: "hidden"});
+            html.css({overflow: "hidden"});
         } else {
-            body.removeClass("hidden");
-            html.removeClass("hidden");
+            body.css({overflow: "visible"});
+            html.css({overflow: "visible"});
         }
     }
 
     function _page_loading(is_loading) {
-        var content = $("#content");
-        var main_loader = $("#main-loader");
-
-        _page_hidden(is_loading);
+        // var content = $("#content");
+        var main_loader = $("#main-loader-container");
 
         if (!is_loading) {
-            content.removeClass("d-none");
-            main_loader.addClass("d-none");
+            // content.removeClass("d-none");
+            // main_loader.addClass("fadeOut animated");
+            main_loader.animateCss('fadeOut', function () {
+                main_loader.remove();
+                _page_overflow_hidden(false);
+            });
         }
     }
 
     nav_link_as_icon.bind("click", function () {
         $(this).data("is-open", !$(this).data("is-open"));
 
-        _page_hidden($(this).data("is-open"));
+        _page_overflow_hidden($(this).data("is-open"));
 
         if ($(this).data("is-open")) {
             header_page.addClass("header-page-open");
@@ -86,5 +115,5 @@
 
     setTimeout(function () {
         _page_loading(false);
-    }, 2000);
+    }, 1000);
 })();
