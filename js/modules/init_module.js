0bd6977639f8f2e83c5442f86f6cb1d8a9c0a7d3
alsanchez86 <sanchezpizarro86@gmail.com>
2018-09-27 12:54:53 +0200

    init module

--- js/modules/init_module.js
+++ js/modules/init_module.js
@@ -1,66 +1,61 @@
 define(["jquery", "jquery_cache"], function ($, _$) {
-    // Add in a JS independent
-    var body = $("body");
-    var html = $("html");
-    var nav_link_as_icon = $("#nav-link-menu-icon");
-    var header_page = $("#header-page");
+    function __page_overflow_hidden(is_hidden) {
+        var body = _$("body");
+        var html = _$("html");
+        var hidden = is_hidden ? "hidden" : "visible";
 
-    function _page_overflow_hidden(is_hidden) {
-        if (is_hidden) {
-            body.css({
-                overflow: "hidden"
-            });
-            html.css({
-                overflow: "hidden"
-            });
-        } else {
-            body.css({
-                overflow: "visible"
-            });
-            html.css({
-                overflow: "visible"
-            });
-        }
+        body.css({
+            overflow: hidden
+        });
+        html.css({
+            overflow: hidden
+        });
     }
 
-    function _page_loading(is_loading) {
-        // var content = $("#content");
-        var main_loader = $("#main-loader-container");
+    function __page_loading(is_loading) {
+        var main_loader = _$("#main-loader-container");
 
         if (!is_loading) {
-            // content.removeClass("d-none");
-            // main_loader.addClass("fadeOut animated");
             main_loader.animateCss("fadeOut", function () {
                 main_loader.remove();
-                _page_overflow_hidden(false);
+                __page_overflow_hidden(false);
             });
         }
     }
 
-    nav_link_as_icon.bind("click", function () {
-        $(this).data("is-open", !$(this).data("is-open"));
+    function __set_active_menu_link() {
+        var pathname = window.location.pathname;
+        var path_split = pathname.split("/");
+        var path_id = path_split && path_split[1];
 
-        _page_overflow_hidden($(this).data("is-open"));
+        _$("#main-nav .nav-link").removeClass("active");
 
-        if ($(this).data("is-open")) {
-            header_page.addClass("header-page-open").animateCss("fadeInDown faster");
+        if (path_id !== "") {
+            _$("#" + path_id).addClass("active");
         } else {
-            header_page.removeClass("header-page-open");
+            _$("#inicio").addClass("active");
         }
-    });
-
-    // Set active menu link
-    var pathname = window.location.pathname;
-    var path_split = pathname.split("/");
-    var path_id = path_split && path_split[1];
-
-    $("#main-nav .nav-link").removeClass("active");
-
-    if (path_id !== "") {
-        $("#" + path_id).addClass("active");
-    } else {
-        $("#inicio").addClass("active");
     }
 
-    _page_loading(false);
+    function __mobile_menu_xs() {
+        var nav_link_as_icon = _$("#nav-link-menu-icon");
+        var header_page = _$("#header-page");
+
+        nav_link_as_icon.bind("click", function () {
+            $(this).data("is-open", !$(this).data("is-open"));
+
+            __page_overflow_hidden($(this).data("is-open"));
+
+            if ($(this).data("is-open")) {
+                header_page.addClass("header-page-open").animateCss("fadeInDown faster");
+            } else {
+                header_page.removeClass("header-page-open");
+            }
+        });
+    }
+
+    // INIT MODULE
+    __set_active_menu_link();
+    __mobile_menu_xs();
+    __page_loading(false);
 });
