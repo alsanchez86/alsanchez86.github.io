55ea954bc596c33a40b95f7a39e9489f33ef02ad
alsanchez86 <sanchezpizarro86@gmail.com>
2018-10-03 22:21:48 +0200

    default update

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@ gulp
 git add .
 git commit -m "default update"
 git push
-git config --global credential.helper 'cache --timeout=360000000000'
+# git config --global credential.helper 'cache --timeout=360000000000'
