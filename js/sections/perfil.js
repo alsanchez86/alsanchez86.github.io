129509e8e655eb9a4ab725928cc62fdbd9cfaa40
alsanchez86 <sanchezpizarro86@gmail.com>
2019-03-14 00:54:29 +0100

    default update

--- js/sections/perfil.js
+++ js/sections/perfil.js
@@ -7,112 +7,116 @@ define(function () {
                 // jquery document ready
                 // domReady requirejs plugin is not neccesary here
                 $(function () {
-                    require(["init", "d3", "d3pie"], function (init, d3, d3pie) {
+                    require(["init"], function (init) {
                         init.set_active_menu_link();
                         init.mobile_menu_xs();
                         init.page_loading(false);
 
-                        var pie = new d3pie("pieChart", {
-                            "header": {
-                                "title": {
-                                    "text": "Spousal Resentment",
-                                    "fontSize": 22,
-                                    "font": "verdana"
+                        require(["d3"], function (d3) {
+                            var pie = new d3pie("pieChart", {
+                                "header": {
+                                    "title": {
+                                        "text": "Spousal Resentment",
+                                        "fontSize": 22,
+                                        "font": "verdana"
+                                    },
+                                    "subtitle": {
+                                        "text": "Comments my wife has made when I tell her I'm working on this script instead of doing something \"fun\".",
+                                        "color": "#999999",
+                                        "fontSize": 10,
+                                        "font": "verdana"
+                                    },
+                                    "titleSubtitlePadding": 12
                                 },
-                                "subtitle": {
-                                    "text": "Comments my wife has made when I tell her I'm working on this script instead of doing something \"fun\".",
+                                "footer": {
+                                    "text": "Source: me, my room, the last couple of months.",
                                     "color": "#999999",
-                                    "fontSize": 10,
-                                    "font": "verdana"
+                                    "fontSize": 11,
+                                    "font": "open sans",
+                                    "location": "bottom-center"
                                 },
-                                "titleSubtitlePadding": 12
-                            },
-                            "footer": {
-                                "text": "Source: me, my room, the last couple of months.",
-                                "color": "#999999",
-                                "fontSize": 11,
-                                "font": "open sans",
-                                "location": "bottom-center"
-                            },
-                            "size": {
-                                "canvasHeight": 400,
-                                "canvasWidth": 590,
-                                "pieOuterRadius": "88%"
-                            },
-                            "data": {
-                                "content": [
-                                    {
-                                        "label": "When's it going to be done?",
-                                        "value": 8,
-                                        "color": "#7e3838"
+                                "size": {
+                                    "canvasHeight": 400,
+                                    "canvasWidth": 590,
+                                    "pieOuterRadius": "88%"
+                                },
+                                "data": {
+                                    "content": [
+                                        {
+                                            "label": "When's it going to be done?",
+                                            "value": 8,
+                                            "color": "#7e3838"
+                                        },
+                                        {
+                                            "label": "Bennnnn!",
+                                            "value": 5,
+                                            "color": "#7e6538"
+                                        },
+                                        {
+                                            "label": "Oh, god.",
+                                            "value": 2,
+                                            "color": "#7c7e38"
+                                        },
+                                        {
+                                            "label": "But it's Friday night!",
+                                            "value": 3,
+                                            "color": "#587e38"
+                                        },
+                                        {
+                                            "label": "Again?",
+                                            "value": 2,
+                                            "color": "#387e45"
+                                        },
+                                        {
+                                            "label": "I'm considering an affair.",
+                                            "value": 1,
+                                            "color": "#387e6a"
+                                        },
+                                        {
+                                            "label": "[baleful stare]",
+                                            "value": 3,
+                                            "color": "#386a7e"
+                                        }
+                                    ]
+                                },
+                                "labels": {
+                                    "outer": {
+                                        "pieDistance": 32
                                     },
-                                    {
-                                        "label": "Bennnnn!",
-                                        "value": 5,
-                                        "color": "#7e6538"
+                                    "inner": {
+                                        "format": "value"
                                     },
-                                    {
-                                        "label": "Oh, god.",
-                                        "value": 2,
-                                        "color": "#7c7e38"
+                                    "mainLabel": {
+                                        "font": "verdana"
                                     },
-                                    {
-                                        "label": "But it's Friday night!",
-                                        "value": 3,
-                                        "color": "#587e38"
+                                    "percentage": {
+                                        "color": "#e1e1e1",
+                                        "font": "verdana",
+                                        "decimalPlaces": 0
                                     },
-                                    {
-                                        "label": "Again?",
-                                        "value": 2,
-                                        "color": "#387e45"
+                                    "value": {
+                                        "color": "#e1e1e1",
+                                        "font": "verdana"
                                     },
-                                    {
-                                        "label": "I'm considering an affair.",
-                                        "value": 1,
-                                        "color": "#387e6a"
+                                    "lines": {
+                                        "enabled": true,
+                                        "color": "#cccccc"
                                     },
-                                    {
-                                        "label": "[baleful stare]",
-                                        "value": 3,
-                                        "color": "#386a7e"
+                                    "truncation": {
+                                        "enabled": true
                                     }
-                                ]
-                            },
-                            "labels": {
-                                "outer": {
-                                    "pieDistance": 32
                                 },
-                                "inner": {
-                                    "format": "value"
-                                },
-                                "mainLabel": {
-                                    "font": "verdana"
-                                },
-                                "percentage": {
-                                    "color": "#e1e1e1",
-                                    "font": "verdana",
-                                    "decimalPlaces": 0
-                                },
-                                "value": {
-                                    "color": "#e1e1e1",
-                                    "font": "verdana"
-                                },
-                                "lines": {
-                                    "enabled": true,
-                                    "color": "#cccccc"
-                                },
-                                "truncation": {
-                                    "enabled": true
+                                "effects": {
+                                    "pullOutSegmentOnClick": {
+                                        "effect": "linear",
+                                        "speed": 400,
+                                        "size": 8
+                                    }
                                 }
-                            },
-                            "effects": {
-                                "pullOutSegmentOnClick": {
-                                    "effect": "linear",
-                                    "speed": 400,
-                                    "size": 8
-                                }
-                            }
+                            });
                         });
+
+                        
 
 
 
