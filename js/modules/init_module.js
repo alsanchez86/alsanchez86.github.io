5fb92f066ac6df26fd177c9a415252fb5a6809d6
alsanchez86 <sanchezpizarro86@gmail.com>
2019-09-10 18:02:25 +0200

    default update

--- js/modules/init_module.js
+++ js/modules/init_module.js
@@ -1,7 +1,8 @@
 define(["module", "jquery", "jquery_cache_module"], function (module, $, _$) {
-
     /**
-     *
+     * Set page loading to true or false
+     * If true, preloader will be showed
+     * If false, preloader will be hide with a fadeOut effect
      */
     module.exports.setPageLoading = function (is_loading) {
         var $preLoader = _$("#preloader");
