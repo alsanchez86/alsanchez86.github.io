a403a4429d7ed66a64c359724e621e7f3655a66e
alsanchez86 <sanchezpizarro86@gmail.com>
2019-09-15 18:52:00 +0200

    default update

--- js/modules/profile_module.js
+++ js/modules/profile_module.js
@@ -2,30 +2,36 @@ define(["module", "jquery_cache_module"], function (module, _$) {
     var interval;
     var intervalDelay = 1000;
 
+    function _animateProgress() {
+        var animateClass = "progress-wrapper-animated";
+        var _$wrappers = _$(".progress-wrapper");
+        var top = _$("window", window).scrollTop();
+
+        _$wrappers.each(function (index, domElem) {
+            var id = ("#" + domElem.id);
+            var isVisible = (_$(id).scrollTop() < top);
+            var hasClass = _$(id).hasClass(animateClass);
+
+            if (isVisible && !hasClass) {
+                _$(id).addClass(animateClass);
+            }
+        });
+    }
+
     /**
      *
      * @return {void}
      * */
     module.exports.animateProgressBar = function () {
-        var animateClass = "progress-wrapper-animated";
-        var _$wrappers = _$(".progress-wrapper");
-
+        // Exec on init
+        _animateProgress();
+        // Create listener
         _$("window", window).scroll(function () {
             if (interval) {
                 clearTimeout(interval);
             }
-
             interval = setTimeout(function () {
-                var top = _$("window").scrollTop();
-                _$wrappers.each(function (index, domElem) {
-                    var id = ("#" + domElem.id);
-                    var isVisible = (_$(id).scrollTop() < top);
-                    var hasClass = _$(id).hasClass(animateClass);
-
-                    if (isVisible && !hasClass) {
-                        _$(id).addClass(animateClass);
-                    }
-                });
+                _animateProgress();
             }, intervalDelay);
         });
     }
