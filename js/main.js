bc952e43e98180d5da71c9d6a70804cc7957a03a
alsanchez86 <sanchezpizarro86@gmail.com>
2019-09-17 19:42:55 +0200

    default update

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ define(function () {
                 $(function () {
                     require(["init_module", "profile_module"], function (init_module, profile_module) {
                         init_module.setPageLoading(false, function () {
-                            profile_module.animateProgressBar();
+                            // profile_module.animateProgressBar();
                         });
                     });
                 });
