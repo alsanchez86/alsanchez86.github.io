84a8c841f79d96bf536dbee1de16d930dea27c72
alsanchez86 <sanchezpizarro86@gmail.com>
2019-10-12 17:53:11 +0200

    default update

--- js/modules/perfil_module.js
+++ js/modules/perfil_module.js
@@ -22,8 +22,8 @@ define(["module", "jquery_cache_module", "glide"], function (module, _$, Glide) 
 
     function initSliders() {
         ["expertus", "innofis", "ximdex", "existo", "freelance", "gaesa"]
-            .map(function (company) {
-                return new Glide("#glide-slider-" + company)
+            .map(function (id) {
+                return new Glide("#glide-slider-" + id)
                     .mount({
                         type: "carousel",
                         hoverpause: true,
