0c332a31f99f857df9849750623ec7c81a176fb0
alsanchez86 <sanchezpizarro86@gmail.com>
2019-10-12 12:59:21 +0200

    default update

--- js/require-config.js
+++ js/require-config.js
@@ -19,7 +19,7 @@ require.config(
                 // lib
                 jquery: _this.config.lib + "jquery.min",
                 bootstrap: _this.config.lib + "bootstrap.bundle.min",
-                tiny_slider: _this.config.lib + "tiny-slider.js",
+                tiny_slider: _this.config.lib + "tiny-slider",
                 // modules
                 jquery_cache_module: _this.config.modules + "jquery_cache_module",
                 main_module: _this.config.modules + "main_module",
