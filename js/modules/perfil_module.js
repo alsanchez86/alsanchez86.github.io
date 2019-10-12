5b8d6db174abad13306a592b04c67efbc8449109
alsanchez86 <sanchezpizarro86@gmail.com>
2019-10-12 18:07:44 +0200

    default update

--- js/modules/perfil_module.js
+++ js/modules/perfil_module.js
@@ -26,6 +26,7 @@ define(["module", "jquery_cache_module", "glide"], function (module, _$, Glide) 
                 return new Glide("#glide-slider-" + id, {
                     type: "carousel",
                     perView: 2,
+                    anchors: true
                     // hoverpause: true,
                     // autoplay: 2000
                 }).mount();
