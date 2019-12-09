51c1d034720ee5b0c92f9658ab1c3c1ec01b3728
alsanchez86 <sanchezpizarro86@gmail.com>
2019-12-09 13:49:18 +0100

    default update

--- js/main.js
+++ js/main.js
@@ -2,25 +2,29 @@
 
 define(function () {
     require(["require-config"], function () {
-        require(["jquery"], function ($) {
-            require(["bootstrap"], function () {
-                // jquery document ready
-                // domReady requirejs plugin is not necessary here
-                $(function () {
-                    // Section module
-                    if (sectionModule && (typeof sectionModule === "string") && (sectionModule !== "")) {
-                        require(["main_module", sectionModule], function (main_module, _sectionModule_) {
-                            mainPageLoading(main_module, function () {
-                                if (_sectionModule_ && _sectionModule_.run && (typeof _sectionModule_.run === "function")) {
-                                    _sectionModule_.run();
-                                }
+        require(["fetch"], function (fetch) {
+            window.fetch = fetch;
+
+            require(["jquery"], function ($) {
+                require(["bootstrap"], function () {
+                    // jquery document ready
+                    // domReady requirejs plugin is not necessary here
+                    $(function () {
+                        // Section module
+                        if (sectionModule && (typeof sectionModule === "string") && (sectionModule !== "")) {
+                            require(["main_module", sectionModule], function (main_module, _sectionModule_) {
+                                mainPageLoading(main_module, function () {
+                                    if (_sectionModule_ && _sectionModule_.run && (typeof _sectionModule_.run === "function")) {
+                                        _sectionModule_.run();
+                                    }
+                                });
                             });
-                        });
-                    } else {
-                        require(["main_module"], function (main_module) {
-                            mainPageLoading(main_module);
-                        });
-                    }
+                        } else {
+                            require(["main_module"], function (main_module) {
+                                mainPageLoading(main_module);
+                            });
+                        }
+                    });
                 });
             });
         });
