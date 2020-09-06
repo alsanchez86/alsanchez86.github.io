85e81c7d12cd501fa816c910ba3739a1a1b4863b
Angel Luis Sanchez Pizarro <angel.luis.sanchez.pizarro@everis.com>
2020-09-06 19:36:16 +0200

    upgrade gulp to 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,130 +1,15 @@
 "use strict";
 
+const fs = require("fs");
+const pkg = JSON.parse(fs.readFileSync("./package.json"));
 const gulp = require("gulp");
+const { series } = require('gulp');
+const concat = require("gulp-concat");
 const sass = require("gulp-sass");
 const cleanCSS = require("gulp-clean-css");
-const concat = require("gulp-concat");
 const rename = require("gulp-rename");
-const rm = require("gulp-rm");
-const runSequence = require("run-sequence");
-const fs = require("fs");
 const mustache = require("gulp-mustache");
 const htmlmin = require("gulp-htmlmin");
-const pkg = JSON.parse(fs.readFileSync("./package.json"));
-
-/*
- * Css tasks
- */
-
-gulp.task("css:reset", function () {
-    return gulp.src(pkg.css + "**/*", {
-            read: false
-        })
-        .pipe(rm())
-});
-
-gulp.task("css:sass", function () {
-    return gulp.src([pkg.sass + "main.scss"])
-        .pipe(sass.sync().on("error", sass.logError))
-        .pipe(concat("sass.css"))
-        .pipe(gulp.dest(pkg.css));
-});
-
-gulp.task("css:lib", function () {
-    return gulp.src([
-            pkg.node_modules + "font-awesome/css/font-awesome.css",
-            pkg.node_modules + "@glidejs/glide/dist/css/glide.core.min.css"
-        ])
-        .pipe(concat("lib.css"))
-        .pipe(gulp.dest(pkg.css));
-});
-
-gulp.task("css:concat", function () {
-    return gulp.src([
-            pkg.css + "sass.css",
-            pkg.css + "lib.css"
-        ])
-        .pipe(concat("main.css"))
-        .pipe(gulp.dest(pkg.css));
-});
-
-gulp.task("css:min", function () {
-    return gulp.src([pkg.css + "main.css"])
-        .pipe(cleanCSS({
-            compatibility: "ie8"
-        }))
-        .pipe(rename({
-            suffix: ".min"
-        }))
-        .pipe(gulp.dest(pkg.css));
-});
-
-gulp.task("css:clean", function () {
-    return gulp.src([pkg.css + "lib.css", pkg.css + "sass.css"], {
-            read: false
-        })
-        .pipe(rm())
-});
-
-/*
- * Js tasks
- */
-
-gulp.task("js:lib", function () {
-    return gulp.src([
-            pkg.node_modules + "requirejs/require.js",
-            pkg.node_modules + "jquery/dist/jquery.min.js",
-            pkg.node_modules + "bootstrap/dist/js/bootstrap.bundle.min.js",
-            pkg.node_modules + "@glidejs/glide/dist/glide.js"
-        ])
-        .pipe(gulp.dest(pkg.lib));
-});
-
-/*
- * Template tasks
- */
-
-gulp.task("templates", [], function (done) {
-    const section = ((process && process.argv && process.argv[3]) || "");
-    const root = [".", "src", "templates", "sections", section].filter(e => e).join("/").concat("/");
-    const dirs = readDirs(root);
-
-    dirs.push(root); // push root index
-    dirs.map(dir => {
-        let outputDir = dir.replace(root, "./");
-        gulp.src(dir + "index.mustache")
-            .pipe(mustache(dir +  "index.json", {
-                extension: ".html"
-            }, {}))
-            .pipe(htmlmin({
-                collapseWhitespace: true
-            }))
-            .pipe(gulp.dest(outputDir))
-    });
-});
-
-/*
- * Environments tasks
- */
-
-// prod
-gulp.task("prod", [], function (done) {
-    runSequence("css:reset", "css:sass", "css:lib", "css:concat", "css:min", "css:clean", "js:lib", "templates", function () {
-        done();
-    });
-});
-
-// default
-gulp.task("default", ["prod"]);
-
-/*
- * Watchers
- */
-gulp.task("watch", ["default"], function () {
-    gulp.watch([
-        pkg.sass + "**/*.scss"
-    ], ["default"]);
-});
 
 /**
  * Read all subfolders from source folder
@@ -139,3 +24,85 @@ function readDirs(source) {
         .map(dir => new Array(dir).concat(readDirs(dir)))
         .reduce((acc, val) => acc.concat(val), []);
 }
+
+/*
+ * Css tasks
+ */
+function sassCompile (cb){
+    gulp.src([pkg.sass + "main.scss"])
+        .pipe(sass.sync().on("error", sass.logError))
+        .pipe(concat("sass.css"))
+        .pipe(gulp.dest(pkg.css));
+    cb();
+}
+
+function cssLibs (cb){
+    gulp.src([
+            pkg.node_modules + "font-awesome/css/font-awesome.css",
+            pkg.node_modules + "@glidejs/glide/dist/css/glide.core.min.css"
+        ])
+        .pipe(concat("lib.css"))
+        .pipe(gulp.dest(pkg.css));
+    cb();
+}
+
+function cssConcat (cb){
+    gulp.src([
+            pkg.css + "sass.css",
+            pkg.css + "lib.css"
+        ], { allowEmpty: true })
+        .pipe(concat("main.css"))
+        .pipe(gulp.dest(pkg.css));
+    cb();
+}
+
+function cssMin (cb) {
+    gulp.src([pkg.css + "main.css"])
+        .pipe(cleanCSS({
+            compatibility: "ie8"
+        }))
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest(pkg.css));
+    cb();
+}
+
+/*
+ * Js tasks
+ */
+function jsLibs (cb){
+    gulp.src([
+            `${pkg.node_modules}requirejs/require.js`,
+            `${pkg.node_modules}jquery/dist/jquery.min.js`,
+            `${pkg.node_modules}bootstrap/dist/js/bootstrap.bundle.min.js`,
+            `${pkg.node_modules}@glidejs/glide/dist/glide.js`
+        ])
+        .pipe(gulp.dest(pkg.lib));
+    cb();
+}
+
+/*
+ * Template tasks
+ */
+function templates (cb) {
+    const section = ((process && process.argv && process.argv[3]) || "");
+    const root = [".", "src", "templates", "sections", section].filter(e => e).join("/").concat("/");
+    const dirs = readDirs(root);
+
+    dirs.push(root); // push root index
+    dirs.map((dir) => {
+        let outputDir = dir.replace(root, "./");
+        gulp.src(dir + "index.mustache")
+            .pipe(mustache(dir +  "index.json", {
+                extension: ".html"
+            }, {}))
+            .pipe(htmlmin({
+                collapseWhitespace: true
+            }))
+            .pipe(gulp.dest(outputDir))
+    });
+    cb();
+}
+
+exports.default = series(sassCompile, cssLibs, cssConcat, cssMin, jsLibs, templates);
