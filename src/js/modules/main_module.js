30bd908ff0578139fc1e38be6b302672276b0d0e
Angel Luis Sanchez Pizarro <angel.luis.sanchez.pizarro@everis.com>
2022-06-17 18:50:23 +0200

    default update

--- src/js/modules/main_module.js
+++ src/js/modules/main_module.js
@@ -13,9 +13,13 @@ define(['module', 'jquery_cache_module'], function (module, _$) {
   module.exports.setPageLoading = function (is_loading, callback) {
     var _callback = typeof callback === 'function' ? callback : function () {};
     var _$preLoader = _$('#preloader');
+    var _$body = _$('body');
 
     if (!is_loading) {
-      _$preLoader.delay(200).fadeOut('slow', _callback);
+      _$preLoader.delay(100).fadeOut('slow', function () {
+        _$body.removeClass('hidden');
+        _callback();
+      });
     }
   };
 
